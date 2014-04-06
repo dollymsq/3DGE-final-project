161c9d80a5f70f772015321fa626b493c4d8a3a2
Timothy Parsons <taparson@cs.brown.edu>
2014-04-06 15:21:09 -0400

    more file cleanup

--- moc_mainwindow.cpp
+++ moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created: Sun Apr 6 15:11:05 2014
+** Created: Sun Apr 6 15:18:57 2014
 **      by: The Qt Meta Object Compiler version 63 (Qt 4.8.2)
 **
 ** WARNING! All changes made in this file will be lost!
