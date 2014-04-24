b9acbd7d2b214d8f1b4af2df18c5c60db7e1cb90
Timothy Parsons <taparson@cs.brown.edu>
2014-04-24 17:09:22 -0400

    committing before pull

--- src/math/levent.h
+++ src/math/levent.h
@@ -4,11 +4,13 @@
 #include <glm/geometric.hpp>
 #include <glm/vec3.hpp>
 #include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
 
 struct LEvent  {
    glm::vec3 currentUp,currentLeft,currentDir,currentPos;
-   glm::mat3 currentRot;
+//   glm::mat3 currentRot;
    float currentThickness;
+   glm::mat4 currentRotations;
 };
 
 #endif // LEVENT_H
